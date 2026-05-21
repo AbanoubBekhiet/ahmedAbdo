fb8cbeae6f8c6ff72f729b5761d29009971b92c2
AbanoubBekhiet <abanwbbekhiet@gmail.com>
2026-05-21 19:28:34 +0300

    making the migration files

---
+++ database/migrations/2026_05_21_155749_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("category_id")->constrained('categories')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string("name");
+            $table->text("description")->nullable();
+            $table->decimal("unit_price",10,2);
+            $table->integer("quantity")->default(0);
+            $table->enum("unit",["شريط","كرتونة","علبة"])->default("كرتونة");
+            $table->string("image_path")->nullable();
+            $table->boolean("status")->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2026_05_21_162308_create_carts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id")->constrained("users")->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId("product_id")->constrained("products")->cascadeOnDelete()->cascadeOnUpdate();
+            $table->integer("number_of_units")->default(0);
+            $table->decimal("unit_price",10,2);
+            $table->decimal("total_price",10,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carts');
+    }
+};
